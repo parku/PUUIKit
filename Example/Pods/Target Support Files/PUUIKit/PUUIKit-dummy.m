9b546de8ccfee8678cd54b07b6eab0d687ab6ba5
Danilo <dabio@users.noreply.github.com>
2016-02-16 16:42:00 +0100

    Remove Example/Pods again.

--- Example/Pods/Target Support Files/PUUIKit/PUUIKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PUUIKit : NSObject
-@end
-@implementation PodsDummy_PUUIKit
-@end
--- Example/Pods/Target Support Files/PUUIKit/PUUIKit-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double PUUIKitVersionNumber;
-FOUNDATION_EXPORT const unsigned char PUUIKitVersionString[];
-
